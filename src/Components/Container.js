75786c738465310fd3e8ba13fd714caa35cf9485
Dawid <dawidgalsino@vp.pl>
2019-12-16 20:02:52 +0100

    update: added router and basic paths to the project

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -1,30 +1,30 @@
 import React, { Fragment } from 'react';
-import Section from './Section/Section';
-import SetupStage from './SetupStage/SetupStage';
 
 // Libraries:
 import Normalize from 'react-normalize';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 //Components
 import Header from './Header/Header';
-import Footer from './Footer/Footer';
 import LandingPage from './LandingPage/LandingPage';
+import SetupStage from './SetupStage/SetupStage';
 import QuestionStage from './QuestionStage/QuestionStage';
 import ResultsStage from './ResultsStage/ResultsStage';
+import Footer from './Footer/Footer';
 
 const Container = () => {
   return (
-    <Fragment>
+    <Router>
       <Normalize />
       <Header />
       <main className="main-landing-page">
-        <LandingPage />
-        <SetupStage />
-        <QuestionStage />
-        <ResultsStage />
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/setup-stage" component={SetupStage} />
+        <Route path="/question-stage" component={QuestionStage} />
+        <Route path="/result-stage" component={ResultsStage} />
       </main>
       <Footer />
-    </Fragment>
+    </Router>
   );
 };
 
